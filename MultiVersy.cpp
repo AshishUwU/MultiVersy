8fd76e51df6320959353ea950d5be4c8eae2a369
AshishUwU <92163086+AshishUwU@users.noreply.github.com>
2021-11-10 16:42:26 +0530

    Main File Added

---
+++ MultiVersy.cpp
@@ -0,0 +1,41 @@
+/**************************************************************************************************************
+ This Code is a "Beta"
+ Original Code will be published soon
+ This code can be used with or without modifications in your code
+ But You must give credit
+ This Program is will combine some console programs
+ For Now they are:
+ Calculator
+ Download Manager
+ **************************************************************************************************************/
+#include <iostream>
+#include<cmath>
+#include<math.h>
+#include<limits>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    char in;
+    cout<<"Welcome To MultiVersy!\n";
+    cout<<"Please Enter Input: ";
+    cin>>in;
+switch (in)
+{
+    case 'c':
+    system("Calculator.exe");
+    break;
+    case 'u':
+    system("text.exe");
+    break;
+    case 's':
+    system("C:\\Windows\\System32\\shutdown /s");
+    case 'r':
+    system("C:\\Windows\\System32\\shutdown /r");
+    case 'h':
+    cout<<"Guide:";
+return 0;
+return 0;
+}
+}
